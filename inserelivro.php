ddd4e2627f9f45fa9360d13b882f4422051be437
RicardoSilva94 <ricardojgsilva94@gmail.com>
2023-07-24 19:50:25 +0100

    Adicionei o dialog após registar mostrando por tr ás a pagina Inseri uma avaliação quando insiro o livro. Ainda nao fui buscar os valores ao rating.

--- inserelivro.php
+++ inserelivro.php
@@ -12,6 +12,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id_utilizador = $_SESSION['id_utilizador'];
 
 
+    $data = date('Y-m-d H:i:s');
+    $personagens = filter_input(INPUT_POST, 'personagens');
+    $enredo = filter_input(INPUT_POST, 'enredo');
+    $estilo_de_escrita = filter_input(INPUT_POST, 'estilo_de_escrita');
+    $coerencia = filter_input(INPUT_POST, 'coerencia');
+    $originalidade = filter_input(INPUT_POST, 'originalidade');
+    $desfecho = filter_input(INPUT_POST, 'desfecho');
+    $relevancia = filter_input(INPUT_POST, 'relevancia');
+    $estrutura = filter_input(INPUT_POST, 'estrutura');
+    $detalhe = filter_input(INPUT_POST, 'detalhe');
+
+
+
     /* validar os dados recebidos do formulario */
     if (empty($titulo) || empty($autor)){
         echo "Os campos 'titulo' e 'autor' do formulário devem conter valores ";
@@ -25,27 +38,34 @@ else{
 /* texto sql da iserir*/
 $consulta = "INSERT INTO livros (titulo, autor, categoria, comentario, id_utilizador) VALUES ('$titulo', '$autor', '$categoria', '$comentario', '$id_utilizador' )";
 
-/* ivo: +1 insert into avaliacoes
-id_utilizadoer é do $_SESSION['id_utilizador']
-id_livro vem de $liga->insert_id
-data procurar como fazer php localtime
-
-*/
-
 /* executar a consulta e testar se ocorreu erro */
 if (!$liga->query($consulta)) {
-    echo " Falha ao executar a consulta: \"$consulta\" <br>" . $liga->error;
+    echo " Falha ao inserir livro: \"$consulta\" <br>" . $liga->error;
     $liga->close();  /* fechar a ligação */
+    exit(0);
 }
 else{
     /* percorrer os registos (linhas) da tabela e mostrar na página */
     echo " Novo livro inserido com sucesso" ;
-	echo '<br> <a href="about.php">Voltar para inserir livro </a>';
 }
 
-$avaliacao = "INSERT INTO avaliacoes (	id_utilizador	id_livro	data	personagens	enredo	estilo_de_escrita	coerencia	originalidade	desfecho	relevancia	estrutura	detalhe	
- ) VALUES ('$titulo', '$autor', '$categoria', '$comentario', '$id_utilizador' )";
+$id_livro = $liga->insert_id;
+
+$avaliacao = "INSERT INTO avaliacoes (id_utilizador, id_livro, data, personagens, enredo, estilo_de_escrita, coerencia, originalidade, desfecho, relevancia, estrutura, detalhe) 
+VALUES ('$id_utilizador', '$id_livro', '$data', '1', '2', '3', '4', '5', '6', '7', '8', '9')";
+//$avaliacao = "INSERT INTO avaliacoes(id_utilizador, id_livro, data, personagens, enredo, estilo_de_escrita, coerencia, originalidade, desfecho, relevancia, estrutura, detalhe) VALUES ('$id_utilizador', '$id_livro' '$data', '$personagens', '$enredo', '$estilo_de_escrita', '$coerencia', '$originalidade', '$desfecho', '$relevancia', '$estrutura', '$detalhe' )";
 
 
+if (!$liga->query($avaliacao)){
+    echo " Falha ao avaliar livro: \"$avaliacao\" <br>" . $liga->error;
+    $liga->close();
+    exit(0);
+}
+else{
+    echo "Novo livro avaliado com sucesso" ;
+}
 $liga->close();       /* fechar a ligação */
+
+echo '<br> <a href="about.php">Voltar para inserir livro </a>';
+
 ?>
--- processautilizador.php
+++ processautilizador.php
@@ -1,3 +1,159 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <title>Watercolor | Contact</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="css/spring.css" type="text/css" media="screen" />
+    <!--[if IE 6]>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/ie-hacks.css" />
+    <script type="text/javascript" src="js/DD_belatedPNG.js"></script>
+    <script>/* EXAMPLE */ DD_belatedPNG.fix('*');</script>
+    <![endif]-->
+    <!-- ENDS CSS -->
+    <!-- JS -->
+    <script type="text/javascript" src="js/jquery_1.3.2.js"></script>
+    <script type="text/javascript" src="js/jqueryui.js"></script>
+    <script type="text/javascript" src="js/easing.js"></script>
+    <script type="text/javascript" src="js/jquery.cycle.all.js"></script>
+    <script type="text/javascript" src="js/custom.js"></script>
+    <!-- ENDS JS -->
+    <!-- superfish -->
+    <link rel="stylesheet" type="text/css" media="screen" href="css/superfish-custom.css" />
+    <script type="text/javascript" src="js/superfish-1.4.8/js/hoverIntent.js"></script>
+    <script type="text/javascript" src="js/superfish-1.4.8/js/superfish.js"></script>
+    <!-- ENDS superfish -->
+    <!-- Cufon -->
+    <script src="js/cufon-yui.js" type="text/javascript"></script>
+    <script src="js/bebas_400.font.js" type="text/javascript"></script>
+    <script type="text/javascript">Cufon.replace('.custom', { fontFamily: 'bebas', hover: true });</script>
+    <!-- /Cufon -->
+</head>
+<body>
+<!-- WRAPPER -->
+<div id="wrapper">
+    <!-- navigation -->
+    <ul id="nav" class="sf-menu">
+        <li class="custom"><a href="blog.php">READLIST</a></li>
+        <li class="custom"><a href="gallery.php">LISTA</a></li>
+        <li class="custom"><a href="about.php">LIVROS</a></li>
+        <li class="custom"><a >UTILIZADOR</a>
+            <ul>
+                <li class="custom"><a href="login.php">LOGIN</a></li>
+                <li class="custom"><a href="contact.php">REGISTAR</a></li>
+
+            </ul>
+        </li>
+
+
+    </ul>
+    <!-- ENDS navigation -->
+    <!-- HEADER -->
+    <div id="header">
+        <a href="index.php"><img src="img/logo.png" alt="Logo" id="logo" /></a>
+        <?php
+        session_start();
+        if (isset($_SESSION['nome'])) {
+            echo '<div id="user-info">
+            <div id="user-greeting">Bem-vindo, ' . $_SESSION['nome'] . '!</div>
+            <a href="logout.php" id="logout-link">Logout</a>
+          </div>';
+        }
+        ?>
+        <img src="img/nav-arrow.png" alt="" id="arrow" class="arrow-home" />
+    </div>
+    <!-- ENDS HEADER -->
+    <!-- MAIN -->
+    <div id="main">
+        <p class="section-title"><span class="title custom">CRIAR CONTA</span><span class="desc">Faz já o teu registo!</span></p>
+        <!-- left-content -->
+        <div class="contact-left">
+            <h5 class="custom">Preenche os campos abaixo!</h5>
+            <!-- form -->
+            <form id="formMail" action="processautilizador.php" method="post">
+                <fieldset>
+                    <p>
+                        <label for="name">NOME</label>
+                        <input name="name" id="name" type="text" required/>
+                    </p>
+                    <p>
+                        <label for="username">USERNAME</label>
+                        <input name="username" id="username" type="text" required/>
+                    </p>
+                    <p>
+                        <label for="password">PASSWORD</label>
+                        <input name="password" id="password" type="password" required/>
+                    </p>
+                    <p>
+                        <label for="localidade">LOCALIDADE</label>
+                        <input name="localidade" id="localidade" type="text" required/>
+                    </p>
+                    <p>
+                        <input id="registerButton" type="submit" value="" name="send" />
+                    </p>
+                </fieldset>
+            </form>
+
+            <!-- ENDS form -->
+        </div>
+        <!-- ENDS left-content -->
+        <!-- right-content -->
+
+        <div>
+
+        </div>
+        <!-- ENDS right-content -->
+        <img src="img/conta.png" alt="livros e pc" style="position: relative;
+    top: -50px;">
+
+    </div>
+    <!-- ENDS MAIN -->
+</div>
+<!-- ENDS WRAPPER -->
+<!-- FOOTER -->
+<div id="footer">
+    <div id="footer-wrapper">
+
+        <div class="footer-cols">
+            <div>
+                <ul>
+                    <li><a href="contact.php" class="custom">Registar</a></li>
+
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="about.php" class="custom">Livros</a></li>
+
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="about.php" class="custom">Lista</a></li>
+
+                </ul>
+            </div>
+            <div class="last">
+                <ul>
+                    <li><a href="blog.php" class="custom">ReadList</a></li>
+
+                </ul>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p class="legal">&copy; Copyright 2023 <a href="#">MyRater</a> All Rights Reserved</p>
+        </div>
+    </div>
+</div>
+<!-- ENDS FOOTER -->
+<!-- start cufon -->
+<script type="text/javascript">Cufon.now();</script>
+<!-- ENDS start cufon -->
+</body>
+</html>
+
+
 <?php
 
 global $liga, $liga;
@@ -39,6 +195,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Mensagem de Registo</title>
 </head>
 <body>
+<style>
+    /* Estilize o dialog para centralizá-lo */
+    dialog {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: #f9f9f9;
+        border: 1px solid #ccc;
+        padding: 20px;
+        z-index: 9999;
+    }
+
+    /* Estilize o botão de fechar com a cor vermelha */
+    #fechar-dialog {
+        background-color: red;
+        color: white;
+        border: none;
+        padding: 10px 20px;
+        cursor: pointer;
+    }
+</style>
+
 <dialog id="mensagem-dialog">
     <?php echo $mensagem; ?>
     <button id="fechar-dialog">Fechar</button>
@@ -60,7 +239,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </script>
 
 <script>
-    /* Redirecionar para a página index.html após 5 segundos */
+    /* Redirecionar para a página login.php após 5 segundos */
     setTimeout(function() {
         window.location.href = 'login.php';
     }, 5000); // Tempo em milissegundos (5 segundos)
